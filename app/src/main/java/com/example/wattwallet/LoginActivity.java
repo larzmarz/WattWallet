e749033ef990e7a0a62e9e6674059c498ef676a3
Android <androdavidaguilar@gmail.com>
2023-10-14 23:42:47 -0400

    Completed Login/Front page

--- app/src/main/java/com/example/wattwallet/LoginActivity.java
+++ app/src/main/java/com/example/wattwallet/LoginActivity.java
@@ -8,14 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opening_page);
-        EditText etUsername = findViewById(R.id.etUsername);
-        EditText etPassword = findViewById(R.id.etPassword);
+        setContentView(R.layout.activity_login);
+        TextInputLayout etUsername = findViewById(R.id.etUsername);
+        TextInputLayout etPassword = findViewById(R.id.etPassword);
         Button btLogin = findViewById(R.id.btLogin);
         Button btSignup = findViewById(R.id.btSignup);
         // activate login button
@@ -23,8 +25,8 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // sets the users inputs as username and password values that will be passed to login method
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
+                String username = etUsername.getEditText().getText().toString();
+                String password = etPassword.getEditText().getText().toString();
                 loginUser(username, password);
             }
         });
